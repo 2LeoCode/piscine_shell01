eea895b9f1087c35255a7b145e81dd85d78eff6f
Leo SUARDI <lsuardi@e3r2p21.42.fr>
2020-02-04 17:24:25 +0100

    vraiment?

--- ex03/count_files.sh
+++ ex03/count_files.sh
@@ -1,2 +1,1 @@
-cd ~/documents/piscine_shell01
 find . -not -path '*/\.*' |wc -l
--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,4 +1,4 @@
 let "FT_LINE1 = 30"
 let "FT_LINE2 = 40"
 
-Cat /etc/passwd |sed -n "$FT_LINE1, $FT_LINE2 p" |awk 'NR % 2 != 0' |sed 's/:\(.*\)//g' |rev |sort -r |tr '\n' ', '
+Cat /etc/passwd |sed -n "$FT_LINE1, $FT_LINE2 p" |awk 'NR % 2 != 0' |sed 's/:\(.*\)//g' |rev |sort -r |sed 's/\\n/, /g'
