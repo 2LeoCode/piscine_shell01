a7c3f43be5c9f67c325afd6c1a5457f6e2adcbd8
Leo SUARDI <lsuardi@e3r2p21.42.fr>
2020-02-04 21:09:53 +0100

    la putain de sa race

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,4 +1,4 @@
 let "FT_LINE1 = 30"
 let "FT_LINE2 = 40"
 
-Cat /etc/passwd |sed -n "$FT_LINE1, $FT_LINE2 p" |awk 'NR % 2 != 0' |sed 's/:\(.*\)//g' |rev |sort -r |sed 's/\\n/, /g'
+Cat /etc/passwd |sed '/^#/d' |sed 's/:\(.*\)//g' |sed -n 'n;p' |rev |sort -r |sed -n "$FT_LINE1, $FT_LINE2 p" |tr '\n' ' ' |sed 's/ /, /g' |sed 's/\(.*\),/\1.\%/' |sed 's/\% //'|tr -d '\n'
