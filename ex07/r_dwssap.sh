f034efa5b01fcf71d3e41005d343a753b992cea9
Leo SUARDI <lsuardi@e3r2p21.42.fr>
2020-02-04 21:50:57 +0100

    lacondetesmortsjetencule

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,4 +1,1 @@
-let "FT_LINE1 = 30"
-let "FT_LINE2 = 40"
-
 Cat /etc/passwd |sed '/^#/d' |sed 's/:\(.*\)//g' |sed -n 'n;p' |rev |sort -r |sed -n "$FT_LINE1, $FT_LINE2 p" |tr '\n' ' ' |sed 's/ /, /g' |sed 's/\(.*\),/\1.\%/' |sed 's/\% //'|tr -d '\n'
